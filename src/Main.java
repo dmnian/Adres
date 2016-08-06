44cae35f31a590c76c35aefaf9a6659f938fb923
dmnian <dmnian@gmail.com>
2016-08-06 14:30:30 +0200

    Clean old comments

--- src/Main.java
+++ src/Main.java
@@ -14,43 +14,17 @@ public class Main {
 
         AdressModelList adressModelList = new AdressModelList();
         adressModelList.getImport("import.csv");
-//        adressModelList.getImport(args[1]);
 
         Runnable coordinatesConn = new CoordinatesConn(adressModelList.getaQueue(), adressModelList.getAdressModelList());
-
-
-//        Thread threads_pool[] =  new Thread[numberOfThreads];
-
         ExecutorService pool = Executors.newFixedThreadPool(numberOfThreads);
-
-//        for (int i = 0; i< numberOfThreads; i++){
-//            threads_pool[i] = new Thread(coordinatesConn);
-//        }
 
         for (int i = 0; i < numberOfThreads; i++) {
             pool.submit(coordinatesConn);
         }
-
-
-//        for (int i = 0; i< numberOfThreads; i++){
-//            threads_pool[i].start();
-//             }
-//
-//
-//
-//        for (int i = 0; i< numberOfThreads; i++){
-//            try {
-//                threads_pool[i].join();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-
         pool.shutdown();
 
         DatabaseConn dbConn = new DatabaseConn();
         adressModelList.writeToDatabase(dbConn);
         dbConn.closeConnection();
-
     }
 }
